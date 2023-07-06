ecf46fd7d423e4e2fe0adb0d6b473b05be254f8a
Alexandr <Ale906206@yandex.ru>
2023-07-06 05:37:23 +0300

    feat:разблил проект на 2 модуля, добавил User в Gateway

---
+++ ShareItGateway/src/main/java/ru/practicum/shareit/user/UserController.java
@@ -0,0 +1,39 @@
+package ru.practicum.shareit.user;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.shareit.user.dto.UserDto;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(path = "/users")
+public class UserController {
+    private final UserClient userClient;
+
+    @GetMapping
+    public ResponseEntity<Object> getAllUsers() {
+        return userClient.getAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Object> getUserById(@PathVariable long id) {
+        return userClient.getUserById(id);
+    }
+
+    @PostMapping
+    public ResponseEntity<Object> addUser(@Validated(UserDto.NewUser.class) @RequestBody UserDto user) {
+        return userClient.addUser(user);
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<Object> updateUser(@Validated(UserDto.UpdateUser.class) @RequestBody UserDto user, @PathVariable long id) {
+        return userClient.updateUser(id, user);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> deleteUser(@PathVariable long id) {
+        return userClient.deleteUser(id);
+    }
+}
